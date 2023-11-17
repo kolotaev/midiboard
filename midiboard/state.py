7cb85fade074f911d4623cf1947eb952bdd7e56f
kolotaev <kolotaev@tut.by>
2023-11-17 14:26:54 +0400

    Change array to set

--- midiboard/state.py
+++ midiboard/state.py
@@ -1,7 +1,7 @@
 class State:
     def __init__(self):
         self.on = True
-        self._pressed_keys = {}
+        self._pressed_keys = set()
         # todo - move to config
         self.octave = 0
         self.rand_velocity_enabled = True
@@ -9,10 +9,10 @@ class State:
         self.rand_polytouch_enabled = True # not configurable right now
 
     def add_pressed_key(self, key):
-        self._pressed_keys[key] = True
+        self._pressed_keys.add(key)
 
     def remove_pressed_key(self, key):
-        self._pressed_keys.pop(key, None)
+        self._pressed_keys.remove(key)
 
     def was_key_pressed(self, key):
         return key in self._pressed_keys
